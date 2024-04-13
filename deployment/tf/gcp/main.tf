a769620d660f560525dd8d74f5e52af9674c5f5a
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2024-04-14 00:54:49 +0800

    updated to make dns optional and generic demo script fetch

--- deployment/tf/gcp/main.tf
+++ deployment/tf/gcp/main.tf
@@ -8,48 +8,41 @@ terraform {
 }
 
 
-variable "openai_api_key" {
-  type = string
-  description = "Open AI API Key"
-  sensitive = true
-}
 
 locals {
-  app-name    = "YugabyteDB Samples - YugaPlus"
-  app-machine-name = "YugaPlus"
-  owner       = "yrampuria"
-  owner-email = "yrampuria@yugabyte.com"
-  dept        = "sales"
-  task        = "webinar"
-  expiry      = "2024-04-17_00-00-00_0000"
-  github-repo = "yogendra/YugaPlus"
-  git-branch = "apj-sb"
+  allowed-admin-client = var.allowed-web-client
+  allowed-web-client   = var.allowed-admin-client
+  app-machine-name     = var.app-machine-name
+  app-name             = var.app-name
+  dept                 = var.dept
+  expiry               = var.expiry
+  gcp-cidr             = var.gcp-cidr
+  gcp-disk-size        = var.gcp-disk-size
+  gcp-disk-type        = var.gcp-disk-type
+  gcp-dns-zone         = var.gcp-dns-zone
+  gcp-image-family     = var.gcp-image-family
+  gcp-image-project    = var.gcp-image-project
+  gcp-machine-type     = var.gcp-machine-type
+  gcp-network          = "${local.prefix}-vpc"
+  gcp-project          = var.gcp-project
+  gcp-regions          = var.gcp-regions
+  git-branch           = var.git-branch
+  github-repo          = var.github-repo
+  openai_api_key       = var.openai_api_key
+  owner                = var.owner
+  owner-email          = var.owner-email
+  prefix               = var.prefix
+  task                 = var.task
+  yb-arch              = var.yb-arch
+  yb-release           = var.yb-release
+  yb-version           = split("-", local.yb-release)[0]
+  demo-script-url = var.demo-script-url
+  demo-script-boot-command = var.demo-script-boot-command
 
-  prefix     = "apjsb"
-  yb-version = "2.20.3.0"
-  yb-release = "2.20.3.0-b68"
-  yb-arch    = "x86_64"
-  allowed-web-client = []
-  allowed-admin-client = []
-  openai_api_key = var.openai_api_key
-  gcp-project       = "apj-partner-enablement"
-  gcp-network       = "${local.prefix}-vpc"
-  gcp-image-family  = "ubuntu-2004-lts"
-  gcp-image-project = "ubuntu-os-cloud"
-  gcp-machine-type = "c2-standard-16"
-  gcp-disk-type  ="pd-ssd"
-  gcp-disk-size = "50"
-  gcp-disk-iops = "3000"
-  gcp-regions = {
-    "asia-southeast1" = "Singapore"
-    "asia-south1"     = "Mumbai"
-    "asia-northeast1" = "Tokyo"
-  }
-  gcp-cidr               = "10.99.0.0/16"
-  gcp-dns-zone           = "ws-apj-yugabyte-com"
+  skip_dns = local.gcp-dns-zone == null || local.gcp-dns-zone == ""
 
-  gcp-zones =  { for region, _ in local.gcp-regions: region =>  data.google_compute_zones.az[region].names[0]}
-  zone-preference = "'${join(" ", [ for index, region in keys(local.gcp-regions): "gcp.${region}.${local.gcp-zones[region]}:${index+1}"])}'"
+  gcp-zones              = { for region, _ in local.gcp-regions : region => data.google_compute_zones.az[region].names[0] }
+  zone-preference        = "'${join(" ", [for index, region in keys(local.gcp-regions) : "gcp.${region}.${local.gcp-zones[region]}:${index + 1}"])}'"
   gcp-region-subnet-cidr = { for index, region in keys(local.gcp-regions) : region => cidrsubnet(local.gcp-cidr, 4, index) }
 
   ssh-public-key = tls_private_key.private_key.public_key_openssh
@@ -60,12 +53,13 @@ locals {
     yb_dept   = local.dept
   }
   node-private-ips-by-region = { for region, config in google_compute_address.private-ip : region => config.address }
-  node-private-ips = [ for region, ip in local.node-private-ips-by-region : ip ]
-  node-public-ips = [ for region, config in google_compute_address.public-ip : config.address ]
-  my-ip = "${chomp(data.http.my-ip.response_body)}/32"
-  admin-client-cidrs = concat( local.allowed-admin-client, [ local.gcp-cidr, local.my-ip ])
-  web-client-cidrs = concat( local.allowed-web-client ,local.admin-client-cidrs )
-  demo-node-config = { for region, name in local.gcp-regions: region => <<EOT
+  node-private-ips           = [for region, ip in local.node-private-ips-by-region : ip]
+  node-public-ips            = [for region, config in google_compute_address.public-ip : config.address]
+  my-ip                      = "${chomp(data.http.my-ip.response_body)}/32"
+  admin-client-cidrs         = concat(local.allowed-admin-client, [local.gcp-cidr, local.my-ip])
+  web-client-cidrs           = concat(local.allowed-web-client, local.admin-client-cidrs)
+
+  demo-node-config = { for region, name in local.gcp-regions : region => <<EOT
 export APP_NAME="${local.app-name}"
 export APP_MACHINE_NAME="${local.app-machine-name}"
 export REGION_NAME="${name}"
@@ -80,10 +74,10 @@ export DB_DRIVER_CLASS_NAME="com.yugabyte.Driver"
 export BACKEND_API_KEY="superbowl-2024"
 export PORT="8080"
 export NODE_IP="${google_compute_address.private-ip[region].address}"
-export YB_MASTERS="${join(",", formatlist("%s:7100",local.node-private-ips))}"
-export YB_NODES=( ${join(" ",local.node-private-ips)} )
+export YB_MASTERS="${join(",", formatlist("%s:7100", local.node-private-ips))}"
+export YB_NODES=( ${join(" ", local.node-private-ips)} )
 export YB_FIRST_NODE="${local.node-private-ips[0]}"
-export YB_IS_PRIMARY="${google_compute_address.private-ip[region].address == local.node-private-ips[0]?"TRUE":"FALSE"}"
+export YB_IS_PRIMARY="${google_compute_address.private-ip[region].address == local.node-private-ips[0] ? "TRUE" : "FALSE"}"
 export YB_LOCATION="gcp.${region}.${local.gcp-zones[region]}"
 export YB_VERSION="${local.yb-version}"
 export YB_RELEASE="${local.yb-release}"
@@ -91,11 +85,17 @@ export YB_ARCH="${local.yb-arch}"
 export GITHUB_REPO="${local.github-repo}"
 export GIT_BRANCH="${local.git-branch}"
 export REACT_APP_RUNTIME_ENVIRONMENT="docker"
-export SPRING_FLYWAY_ENABLED="${local.node-private-ips[0] ==  google_compute_address.private-ip[region].address ? "true" : "false"}"
+export SPRING_FLYWAY_ENABLED="${local.node-private-ips[0] == google_compute_address.private-ip[region].address ? "true" : "false"}"
 EOT
   }
 }
 
+provider "google" {
+  alias = "boot"
+}
+
+data "google_client_config" "default" {
+}
 
 provider "google" {
   project        = local.gcp-project
@@ -167,10 +167,11 @@ data "http" "my-ip" {
 }
 
 data "google_dns_managed_zone" "dns" {
+  count = local.skip_dns?0:1
   name = local.gcp-dns-zone
 }
 locals {
-  project-domain = trimsuffix("${local.prefix}.${data.google_dns_managed_zone.dns.dns_name}", ".")
+  project-domain = local.skip_dns ? null : trimsuffix("${local.prefix}.${data.google_dns_managed_zone.dns[0].dns_name}", ".")
 }
 
 
@@ -205,66 +206,67 @@ data "cloudinit_config" "conf" {
       yb-arch        = local.yb-arch
       ssh-public-key = local.ssh-public-key
       region-name    = each.value
-      ssh-key     = tls_private_key.private_key.private_key_openssh
-      ssh-key-pub = tls_private_key.private_key.public_key_openssh
-      demo-env = local.demo-node-config[each.key]
-      github-repo = local.github-repo
-      git-branch  = local.git-branch
+      ssh-key        = tls_private_key.private_key.private_key_openssh
+      ssh-key-pub    = tls_private_key.private_key.public_key_openssh
+      demo-env       = local.demo-node-config[each.key]
+      github-repo    = local.github-repo
+      git-branch     = local.git-branch
+      demo-script-url = local.demo-script-url
+      demo-script-boot-command = local.demo-script-boot-command
     })
     filename = "demo-machine.cloud-init.yaml"
   }
 }
 resource "google_compute_address" "public-ip" {
-  for_each = local.gcp-regions
-  name = "${local.prefix}-${each.key}"
-  region = each.key
+  for_each     = local.gcp-regions
+  name         = "${local.prefix}-${each.key}"
+  region       = each.key
   address_type = "EXTERNAL"
 }
 resource "google_compute_address" "private-ip" {
-  for_each = local.gcp-regions
-  name = "${local.prefix}-${each.key}-pvt"
+  for_each     = local.gcp-regions
+  name         = "${local.prefix}-${each.key}-pvt"
   address_type = "INTERNAL"
-  region = each.key
-  subnetwork = google_compute_subnetwork.subnet[each.key].id
+  region       = each.key
+  subnetwork   = google_compute_subnetwork.subnet[each.key].id
 
 }
 
 resource "google_dns_record_set" "public-fqdn" {
-  for_each = local.gcp-regions
-  managed_zone = data.google_dns_managed_zone.dns.name
-  name = "${each.key}.${local.project-domain}."
-  type = "A"
-  ttl = 30
-  rrdatas = [google_compute_address.public-ip[each.key].address]
+  for_each     = local.skip_dns ? {} : local.gcp-regions
+  managed_zone = data.google_dns_managed_zone.dns[0].name
+  name         = "${each.key}.${local.project-domain}."
+  type         = "A"
+  ttl          = 30
+  rrdatas      = [google_compute_address.public-ip[each.key].address]
 }
 
 
 resource "google_dns_record_set" "private-fqdn" {
-  for_each = local.gcp-regions
-  managed_zone = data.google_dns_managed_zone.dns.name
-  name = "${each.key}-pvt.${local.project-domain}."
-  type = "A"
-  ttl = 30
-  rrdatas = [google_compute_address.private-ip[each.key].address]
+  for_each     = local.skip_dns ? {} : local.gcp-regions
+  managed_zone = data.google_dns_managed_zone.dns[0].name
+  name         = "${each.key}-pvt.${local.project-domain}."
+  type         = "A"
+  ttl          = 30
+  rrdatas      = [google_compute_address.private-ip[each.key].address]
 }
 
 data "google_compute_zones" "az" {
   for_each = local.gcp-regions
-  region = each.key
+  region   = each.key
 }
 
 resource "google_compute_instance" "vm" {
-  for_each = local.gcp-regions
+  for_each     = local.gcp-regions
   name         = local.nodes[each.key].name
   machine_type = local.gcp-machine-type
   zone         = local.nodes[each.key].zone
-  hostname = local.nodes[each.key].hostname
+  hostname     = local.nodes[each.key].hostname
 
   boot_disk {
     initialize_params {
       image = data.google_compute_image.vm-image.id
-      size = local.gcp-disk-size
-      # provisioned_iops = local.gcp-disk-iops
+      size  = local.gcp-disk-size
       type = local.gcp-disk-type
     }
 
@@ -281,57 +283,78 @@ resource "google_compute_instance" "vm" {
   metadata = {
     user-data = data.cloudinit_config.conf[each.key].rendered
   }
-  labels = merge(local.labels,{})
-  tags = [ "demo-machine"]
-   allow_stopping_for_update = true
+  labels                    = merge(local.labels, {})
+  tags                      = ["demo-machine"]
+  allow_stopping_for_update = true
 
 }
 
 
 locals {
-   nodes = {
-    for index, region in keys(local.gcp-regions):
-      region => {
-        name = "${local.prefix}-${region}"
-        preference = index + 1
-        zone = data.google_compute_zones.az[region].names[0]
-        hostname = trimsuffix(google_dns_record_set.public-fqdn[region].name,".")
-        private = {
-          ip = google_compute_address.private-ip[region].address
-          fqdn = trimsuffix(google_dns_record_set.private-fqdn[region].name, ".")
-        }
-        public = {
-          ip = google_compute_address.public-ip[region].address
-          fqdn = trimsuffix(google_dns_record_set.public-fqdn[region].name,".")
-        }
+  nodes = {
+    for index, region in keys(local.gcp-regions) :
+    region => {
+      name       = "${local.prefix}-${region}"
+      preference = index + 1
+      zone       = data.google_compute_zones.az[region].names[0]
+      hostname   = local.skip_dns ? "${region}.local"  : trimsuffix(google_dns_record_set.public-fqdn[region].name, ".")
+      private = {
+        ip   = google_compute_address.private-ip[region].address
+        fqdn = local.skip_dns ? "" : trimsuffix(google_dns_record_set.private-fqdn[region].name, ".")
       }
+      public = {
+        ip   = google_compute_address.public-ip[region].address
+        fqdn = local.skip_dns ? "" : trimsuffix(google_dns_record_set.public-fqdn[region].name, ".")
+      }
+    }
   }
 }
 
 output "vms" {
   value = <<VMS
-%{ for region, config in local.nodes ~}
+%{~ for region, config in local.nodes ~}
+
 ==> Region: ${region}(${local.gcp-regions[region]}) <==============
+
       VM: ${google_compute_instance.vm[region].name}
  Private: ${config.private.ip}
+          %{~ if !local.skip_dns ~}
           ${config.private.fqdn}
+          %{~ endif ~}
   Public: ${config.public.ip}
+          %{~ if !local.skip_dns ~}
           ${config.public.fqdn}
+          %{~ endif ~}
   YB Web: http://${config.public.ip}:15433/
+          %{~ if !local.skip_dns ~}
           http://${config.public.fqdn}:15433/
+          %{~ endif ~}
   Master: http://${config.public.ip}:7000/
+          %{~ if !local.skip_dns ~}
           http://${config.public.fqdn}:7000/
+          %{~ endif ~}
  Tserver: http://${config.public.ip}:9000/
+          %{~ if !local.skip_dns ~}
           http://${config.public.fqdn}:9000/
+          %{~ endif ~}
  App API: http://${config.public.ip}:8080/
+          %{~ if !local.skip_dns ~}
           http://${config.public.fqdn}:8080
+          %{~ endif ~}
   App UI: http://${config.public.ip}:3000/
+          %{~ if !local.skip_dns ~}
           http://${config.public.fqdn}:3000
+          %{~ endif ~}
      SSH: ssh -i ${local_sensitive_file.ssh-private-key.filename} -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" yugabyte@${config.public.ip}
+          %{~ if !local.skip_dns ~}
           ssh -i ${local_sensitive_file.ssh-private-key.filename} -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" yugabyte@${config.public.fqdn}
+          %{~ endif ~}
           gcloud compute ssh --zone ${config.zone} ${google_compute_instance.vm[region].name}
  Pvt SSH: ssh -i ~/.ssh/id_rsa  -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" yugabyte@${config.private.ip}
+          %{~ if !local.skip_dns ~}
           ssh -i ~/.ssh/id_rsa  -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" yugabyte@${config.private.fqdn}
-%{ endfor ~}
+          %{~ endif ~}
+
+%{~ endfor ~}
 VMS
 }
