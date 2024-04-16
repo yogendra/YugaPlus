a3c84431be71ef6fce40cfa55587028c2cd2731b
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2024-04-16 12:36:20 +0800

    fixed tablespace

--- backend/src/main/resources/V2__create_geo_partitioned_user_library-apj.sql
+++ backend/src/main/resources/V2__create_geo_partitioned_user_library-apj.sql
@@ -45,23 +45,19 @@ PARTITION BY LIST (user_location);
  to a respective geo-aware tablespace.
  */
 CREATE TABLE user_library_asia_ne PARTITION OF user_library(user_id, movie_id, start_watch_time, added_time, user_location, PRIMARY KEY (user_id, movie_id, user_location))
-FOR VALUES IN ('Tokyo', 'Osaka') TABLESPACE usa_east_ts;
+FOR VALUES IN ('Tokyo', 'Osaka') TABLESPACE asia_ne_ts;
 
 CREATE TABLE user_library_asia_s PARTITION OF user_library(user_id, movie_id, start_watch_time, added_time, user_location, PRIMARY KEY (user_id, movie_id, user_location))
-FOR VALUES IN ( 'Ayodhya', 'Bengaluru', 'Chennai', 'Delhi', 'Hyderabad','Mumbai') TABLESPACE usa_central_ts;
+FOR VALUES IN ( 'Ayodhya', 'Bengaluru', 'Chennai', 'Delhi', 'Hyderabad','Mumbai') TABLESPACE asia_s_ts;
 
 CREATE TABLE user_library_asia_se PARTITION OF user_library(user_id, movie_id, start_watch_time, added_time, user_location, PRIMARY KEY (user_id, movie_id, user_location))
-FOR VALUES IN ('Singapore', 'Kuala Lumpur') TABLESPACE usa_west_ts;
+FOR VALUES IN ('Singapore', 'Kuala Lumpur') TABLESPACE asia_se_ts;
 
 CREATE TABLE user_library_default PARTITION OF user_library(user_id, movie_id, start_watch_time, added_time, user_location, PRIMARY KEY (user_id, movie_id, user_location))
 DEFAULT;
 
 INSERT INTO user_account(id, email, PASSWORD, full_name, user_location)
-    VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'user1@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'John Doe', 'New York'),
-('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'user2@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'Emely Smith', 'Chicago'),
-('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13', 'user3@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'Michael Williams', 'Los Angeles'),
-('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a14', 'user4@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'Jessica Brown', 'Boston'),
-('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a15', 'arisa@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'Arisa Izuno', 'Tokyo'),
-('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a16', 'srini@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'Srinivasa Vasu', 'Chennai'),
-('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a17', 'yogi@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'Yogi Rampuria', 'Singapore'),
-('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a18', 'ron4@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'Ron Xing', 'Kuala Lumpur');
+    VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'user1@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'John Doe', 'Tokyo'),
+('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'user2@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'Emely Smith', 'Chennai'),
+('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13', 'user3@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'Michael Williams', 'Singapore'),
+('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a14', 'user4@gmail.com', '$2a$10$s17IziaW1967UZGW/Q8diOqX0qCzABGBykf/BK6xvO/qElLKkWV6a', 'Jessica Brown', 'Mumbai'),
